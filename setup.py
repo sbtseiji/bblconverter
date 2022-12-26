91e902a265b03ec895863da2148cc279bd13b756
SHIBATA Seiji <sbtseiji@users.noreply.github.com>
2022-12-26 14:48:53 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,7 +11,6 @@ setuptools.setup(
   long_description=long_description,
   long_description_content_type="text/markdown",
   url="https://github.com/sbtseiji/bblconverter",
-  requires-python = ">=3.7"
   packages=setuptools.find_packages(),
   classifiers = [
       "Programming Language :: Python :: 3",
